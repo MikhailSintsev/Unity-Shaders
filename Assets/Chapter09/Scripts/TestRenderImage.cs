38c796aa75ad6abe172f2ee5ca1a32ca38c91dc8
Mikhail Sintsev <mikhail.sintsev@gmail.com>
2022-04-13 17:43:47 +0400

    Add an existing project

---
+++ Assets/Chapter09/Scripts/TestRenderImage.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+[ExecuteInEditMode]
+
+public class TestRenderImage : MonoBehaviour
+{
+    #region Variables
+
+    public Shader curShader;
+    public float greyscaleAmount = 1.0f;
+
+    private Material screenMat;
+
+    #endregion
+
+    #region Properties
+
+    Material ScreenMat
+    {
+        get
+        {
+            if ( screenMat == null )
+            {
+                screenMat = new Material( curShader );
+                screenMat.hideFlags = HideFlags.HideAndDontSave;
+            }
+            return screenMat;
+        }
+    }
+    #endregion
+
+    private void Start()
+    {
+        if (!SystemInfo.supportsImageEffects)
+        {
+            enabled = false;
+            return;
+        }
+
+        if (!curShader && !curShader.isSupported)
+            enabled = false;
+    }
+
+    private void OnRenderImage(RenderTexture sourceTexture, RenderTexture destTexture)
+    {
+        if (curShader != null)
+        {
+            ScreenMat.SetFloat("_Luminosity", greyscaleAmount);
+            Graphics.Blit(sourceTexture, destTexture, ScreenMat);
+        }
+        else
+            Graphics.Blit(sourceTexture, destTexture);
+    }
+
+    private void Update()
+    {
+        
+    }
+}
